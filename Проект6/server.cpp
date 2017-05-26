b6c8f13c36c97b8f122e7bec3cb8bb93360f94f3
starkov-isb115 <eric.volovets@gmail.com>
2017-05-26 18:38:56 +0300

    v5
    -пароль
    -частично реализована проверка целостности

--- Проект6/server.cpp
+++ Проект6/server.cpp
@@ -9,7 +9,14 @@
 #include <stdio.h>
 #include <clocale>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <Imagehlp.h>
+#include <tchar.h>
+#include "stdafx.h"
 
+#pragma comment(lib, "Imagehlp.lib")
 #pragma comment (lib, "Ws2_32.lib")
 #pragma comment (lib, "Mswsock.lib")
 #pragma comment (lib, "AdvApi32.lib")
@@ -17,39 +24,48 @@
 #define DEFAULT_BUFLEN 1024
 #define DEFAULT_PORT "1997"
 #define MAX 1000
-#define MAXNAME 10
+#define MAXNAME 100
+
+
+using namespace std;
 
 	SOCKET ConnectSocket;
 	SOCKET ListenSocket;
 	SOCKET* ClientSockets;
-	char UserNames[MAXNAME][MAX];
+	string UserNames[MAX];
 	int CV = 0;
 
 void gAs(int vol)
-{
-	while(1)
-	{
+
+{ char *buffer = new char[1024];
 		for (;; Sleep(75))
-	{
-		char *buffer = new char[1024];
+	{	
 		memset(buffer, 0, sizeof(buffer)); std::printf(buffer);
 		if (recv(ClientSockets[vol], buffer, 1024, NULL))
-		{
+		{  
+			string s = string(buffer); string ans;
+			ans= UserNames[vol];ans += ": ";ans+=s;
+			const char *cstr = ans.c_str();
 			for (int i = 0; i <= CV; i++)
-				send(ClientSockets[i], buffer, strlen(buffer), NULL);
+				send(ClientSockets[i], cstr, strlen(cstr), NULL);
+		}
+		delete buffer;
 		}
 
-		delete buffer;
-	}
-	}
 }
 
-int main(void)
+int main()
 {
+	TCHAR szFullPath[MAX_PATH];
+  DWORD dwFileChecksum = 0, dwRealChecksum = 0;
+
+  ::GetModuleFileName(::GetModuleHandle(NULL), szFullPath, MAX_PATH);
 	setlocale(LC_ALL, "russian");
 		WSAData Wsadata;
 		int res = WSAStartup(MAKEWORD(2, 2), &Wsadata);
-		if (res != 0) { return 0; }
+ ::MapFileAndCheckSum(szFullPath, &dwFileChecksum, &dwRealChecksum);
+  if ( dwFileChecksum!=dwRealChecksum ) exit(1);
+
 
 		ClientSockets = (SOCKET*)calloc(64,sizeof(SOCKET));
 
@@ -58,7 +74,6 @@ int main(void)
 	local_addr.sin_port = htons(1997);
     local_addr.sin_addr.s_addr = 0; 
                                     
- 
 	SOCKET ListenSocket = socket(AF_INET, SOCK_STREAM, NULL);
 	bind(ListenSocket, (sockaddr *)&local_addr, sizeof(local_addr));
 	listen(ListenSocket, SOMAXCONN);
@@ -66,21 +81,28 @@ int main(void)
 	SOCKET ConnectSocket;
 	for (;; Sleep(75))
 	{
-		if( ConnectSocket = accept(ListenSocket, NULL, NULL));
+		if( ConnectSocket = accept(ListenSocket, NULL, NULL))
 		{
+			char *m = new char[1024];
+			for (int i = 0; i < MAXNAME; i++) m[i] = 0; 
 			char gm[MAXNAME]; for (int i = 0; i < MAXNAME; i++) gm[i] = 0;
 			recv(ConnectSocket, gm, MAXNAME, NULL);
-			std::cout << gm;
-			for ( int i = 0; i < MAXNAME; i++ )
-			{ UserNames[CV][i] = gm[i]; }
-
+			cout << gm;
+			char *x[sizeof(gm)] = {0};
+			char *p = strtok(gm, " ");
+			int i = 0;
+			while(p)
+			{
+			 x[i++] = p;
+			 p = strtok(NULL, " ");
+			}
+			UserNames[CV] = string(x[0]);
 			ClientSockets[CV] = ConnectSocket; 
 			CV++;
 			CreateThread(NULL,NULL,(LPTHREAD_START_ROUTINE)gAs,(LPVOID)(CV-1),NULL,NULL);
 		}
 		
 	}
-
 	system("pause");
 	closesocket(ListenSocket);
 	WSACleanup();
